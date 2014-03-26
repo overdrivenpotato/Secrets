1ccc719de55dc9d8ef5c97c7a0791bc387644771
overdrivenpotato <marko.mijalkovic97@gmail.com>
2014-03-26 09:40:35 -0400

    Initial commit

---
+++ Desktop/src/com/github/overdrivenpotato/secretsDesktop/DesktopStarter.java
@@ -0,0 +1,20 @@
+package com.github.overdrivenpotato.secretsDesktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.overdrivenpotato.secretsMain.SecretsGdx;
+
+/**
+ * Created by Marko on 1/16/14.
+ */
+public class DesktopStarter {
+    public static void main(String[] args) {
+        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+        cfg.title = "Secrets Pre-Alpha!";
+        cfg.useGL20 = true;
+        cfg.width = 800;
+        cfg.height = 480;
+        cfg.resizable = false;
+        new LwjglApplication(new SecretsGdx(), cfg);
+    }
+}
---
+++ Main/src/org/github/overdrivenpotato/secrets/underthehood/KeyboardMgr.java
@@ -0,0 +1,37 @@
+package org.github.overdrivenpotato.secrets.underthehood;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+
+public class KeyboardMgr {
+    private int[] movementKeys;
+
+    public KeyboardMgr()
+    {
+        movementKeys = new int[4];
+        movementKeys[0] = Input.Keys.LEFT;
+        movementKeys[1] = Input.Keys.RIGHT;
+        movementKeys[2] = Input.Keys.UP;
+        movementKeys[3] = Input.Keys.DOWN;
+    }
+
+    public boolean keyLeft()
+    {
+        return Gdx.input.isKeyPressed(movementKeys[0]);
+    }
+
+    public boolean keyRight()
+    {
+        return Gdx.input.isKeyPressed(movementKeys[1]);
+    }
+
+    public boolean keyUp()
+    {
+        return Gdx.input.isKeyPressed(movementKeys[2]);
+    }
+
+    public boolean keyDown()
+    {
+        return Gdx.input.isKeyPressed(movementKeys[3]);
+    }
+}
